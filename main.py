05cf3260bee3f40232d4ce4942b9b6618548005f
Jacky Sio <jackysio.24@gmail.com>
2024-01-17 15:50:58 -0800

    timeit for main

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# from costco_price_scraper.price_scraper import price_scraper as ps
+
+# ps.run_price_scraper()
+
+
+import timeit
 from costco_price_scraper.price_scraper import price_scraper as ps
 
-ps.run_price_scraper()
+# Wrap the function call in a lambda function
+wrapped_function = lambda: ps.run_price_scraper()
+
+# Run the timeit function
+execution_time = timeit.timeit(wrapped_function, number=1)
+
+# Print the execution time
+print(f"Execution Time: {execution_time:.2f} seconds")
